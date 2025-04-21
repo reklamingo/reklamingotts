43318073183cb3ca4eee8b56d1cf723edc3f6fba
reklamingo <arifkurtulmuslar@gmail.com>
2025-04-21 17:02:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const textInput = document.getElementById("text-input");
 const charCount = document.getElementById("char-count");
 const speakBtn = document.getElementById("speak-button");
@@ -6,15 +5,17 @@ const downloadBtn = document.getElementById("download-button");
 const audioPlayer = document.getElementById("audio-player");
 
 textInput.addEventListener("input", () => {
-  const maxLength = 250;
-  const currentLength = textInput.value.length;
-  charCount.innerText = currentLength + " / " + maxLength + " karakter";
+  if (textInput.value.length > 250) {
+    textInput.value = textInput.value.slice(0, 250);
+  }
+  charCount.innerText = textInput.value.length + " / 250 karakter";
 });
 
 speakBtn.addEventListener("click", async () => {
   const text = textInput.value;
   const voice = document.getElementById("voice-select").value;
   if (!text) return;
+
   speakBtn.classList.remove("glow");
 
   const res = await fetch("/speak", {
@@ -27,14 +28,13 @@ speakBtn.addEventListener("click", async () => {
   const url = URL.createObjectURL(blob);
   audioPlayer.src = url;
   document.querySelector(".player-wrapper").style.display = "flex";
-  document.querySelector(".player-wrapper").style.justifyContent = "center";
   audioPlayer.play();
   downloadBtn.dataset.url = url;
 });
 
 downloadBtn.addEventListener("click", () => {
   const url = downloadBtn.dataset.url;
-  const filename = prompt("Dosya ismi", "ringo-ses.mp3") || "ringo.mp3";
+  const filename = prompt("Dosya ismi:", "ringo-ses.mp3") || "ringo.mp3";
   const a = document.createElement("a");
   a.href = url;
   a.download = filename;
